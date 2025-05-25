b7fb940741857aedbf632440627772337f0b879b
schneidev <39675729+schneidev@users.noreply.github.com>
2025-05-25 22:25:57 +0200

    add adapter-vercel

--- svelte.config.mjs
+++ svelte.config.mjs
@@ -1,10 +1,10 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from "mdsvex";
+import adapter from "@sveltejs/adapter-vercel";
 
 const config = {
 	kit: { adapter: adapter() },
 	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+	extensions: [".svelte", ".svx"]
 };
 
 export default config;
